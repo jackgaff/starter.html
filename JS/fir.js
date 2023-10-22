15db9b33dc43f46c065219513d2a45d380aeb222
Jack Gaffney <jackgaff@umich.edu>
2023-10-22 18:49:20 -0400

    final

--- JS/fir.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.nav-link').forEach(link => {
-    link.addEventListener('click', () => {
-        document.getElementById('header').classList.toggle('hidden');
-    });
-});
